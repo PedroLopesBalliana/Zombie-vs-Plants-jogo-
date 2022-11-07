50289c9c322337d5eb396e321cce53512295a0fd
pedro <pedrolucaslb@hotmail.com>
2022-11-06 22:03:01 -0300

    dd

--- jogo/Espinhos.cpp
+++ jogo/Espinhos.cpp
@@ -7,6 +7,7 @@ Espinhos::Espinhos()
 		std::cout << "falhou a textura :(" << std::endl;
 	}
 	sprite.setTexture(texture);
+	sprite.scale(sf::Vector2f(2.5,2));
 	sprite.setPosition(sf::Vector2f(500.f, 300.f));
 }
 Espinhos::~Espinhos()
