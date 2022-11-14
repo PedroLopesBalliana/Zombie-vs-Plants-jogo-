9a78cfa27a5058711e1ce8056ccbfe7987360062
pedro <pedrolucaslb@hotmail.com>
2022-11-14 20:44:53 -0300

    Composição do Menu

--- jogo/Menu.cpp
+++ jogo/Menu.cpp
@@ -1,15 +1,21 @@
 #include "Menu.h"
 #include <iostream>
 
-Menu::Menu(GerenciadorGrafico* ge)
+
+Menu::Menu(GerenciadorGrafico* ge):botaoFase1(), botaoFase2(), botaoRanking()
 {
-	if (!texture.loadFromFile("assets/Menu/MenuBg.png"))
+	if (!texture.loadFromFile("assets/Menu/MenuBg1.png"))
 	{
 		std::cout << "falhou a textura :(" << std::endl;
 	}
 	sprite.setTexture(texture);
 	sprite.scale(sf::Vector2f(1, 1.25));
 	sprite.setPosition(sf::Vector2f(0.f, 0.f));
+
+	botaoFase1.mudarPosi(200, 100);
+	botaoFase2.mudarPosi(200, 210);
+	botaoRanking.mudarPosi(260, 320);
+
 	setGerenciadorGrafico(ge);
 	executar();
 }
@@ -28,7 +34,11 @@ void Menu::executar()
 			if (event.type == sf::Event::Closed)
 				geren_graf->getWindow()->close();
 		}
+		geren_graf->limpaJanela();
 		imprimirSe();
+		geren_graf->getWindow()->draw(botaoFase1.getRect());
+		geren_graf->getWindow()->draw(botaoFase2.getRect());
+		geren_graf->getWindow()->draw(botaoRanking.getRect());
 		geren_graf->displayJanela();
 	}
 }
--- jogo/Menu.h
+++ jogo/Menu.h
@@ -4,7 +4,32 @@
 class Menu : public Ente
 {
 private:
+	class Botao
+	{
+	private:
+		sf::RectangleShape botao;
+	public:
+		Botao()
+		{
+			botao.setSize(sf::Vector2f(300.f, 100.f));
+			botao.setFillColor(sf::Color::Blue);
+		}
+		void mudarPosi(int x, int y)
+		{
+			botao.setPosition(x, y);
+		}
+		sf::RectangleShape getRect()
+		{
+			return botao;
+		}
+		~Botao()
+		{
 
+		}
+	};
+	Botao botaoFase1;
+	Botao botaoFase2;
+	Botao botaoRanking;
 public:
 	Menu(GerenciadorGrafico* ge);
 	~Menu();
